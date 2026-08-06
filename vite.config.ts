0dfb2cd6b98c86598594dcd5a2c67d62a60276be
10041860top-ops <developer@10041860top-ops.local>
2026-08-06 16:16:07 +0800

    fix: 設定 GitHub Pages 儲存庫資源路徑 base 為 /top3/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/top3/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
